a5b445d7e7f3a08c00964b51ef24f7c676f9cce5
gaurav sharma <g11.sharma@gmail.com>
2022-06-30 17:09:15 +0200

    AllRestaurants-pages

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Card, Col, Button } from "antd";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
-const RestaurantCard = () => {
-  return <div>RestaurantCard</div>;
-};
+function RestaurantCard({ name, image, cuisine, address }) {
+  return (
+    <Col>
+      <Card title={name} style={{ width: 230, height: 300, margin: 10 }}>
+        <img src={image} height={60} alt="food" />
+        <p>cuisine: {cuisine}</p>
+        <p>address: {address}</p>
+      </Card>
+    </Col>
+  );
+}
 
 export default RestaurantCard;
---
+++ src/pages/AllRestaurants.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import RestaurantCard from "../components/RestaurantCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
+
+const AllRestaurants = () => {
+  const [restaurants, setRestaurants] = useState([]);
+
+  const getAllProjects = () => {
+    axios
+      .get(`${API_URL}/api/restaurant/restaurants`)
+      .then((response) => setRestaurants(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getAllProjects();
+  }, []);
+  console.log(getAllProjects);
+  return (
+    <div>
+      {restaurants.map((restaurant) => (
+        <RestaurantCard {...restaurant} />
+      ))}
+    </div>
+  );
+};
+
+export default AllRestaurants;
